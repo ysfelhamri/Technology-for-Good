8e3d077e029a0a7604d0a31b94138c21517ba1f1
Ayoub <Fanaoui.25@gmail.com>
2025-03-13 22:06:19 +0100

    Add scraping service

--- services/scrapingService.js
+++ services/scrapingService.js
@@ -1,1 +1,12 @@
 //to use the scraper.
+const { scrapePrices } = require("../utils/scraper");
+
+
+const scrapingService = {
+    getPrices: async () => {
+      const prices = await scrapePrices();
+      return prices;
+    },
+  };
+  
+  module.exports = scrapingService;
