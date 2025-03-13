9919cc0630a0b6828eec5abd9cb7daec10fd44d9
Ayoub <Fanaoui.25@gmail.com>
2025-03-13 22:13:09 +0100

    Add price controller

--- controllers/priceController.js
+++ controllers/priceController.js
@@ -1,1 +1,18 @@
 //to handle Http requests
+const scrapingService = require("../services/scrapingService");
+
+
+const priceController = {
+    getPrices: async () =>{
+        try{
+            const prices = await scrapingService.getPrices();
+            resizeBy.render("prices", {prices});
+
+        }catch(error){
+            console.status(500).json({message: error.message});
+        }
+    }
+
+}
+
+module.exports = priceController;
