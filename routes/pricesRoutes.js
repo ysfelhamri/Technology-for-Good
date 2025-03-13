2f7ea53650130c7c5bc0a33597b62bfd6f22c7a0
Ayoub <Fanaoui.25@gmail.com>
2025-03-13 22:18:38 +0100

    Add price routes

--- routes/pricesRoutes.js
+++ routes/pricesRoutes.js
@@ -1,1 +1,9 @@
 //to define  routes
+
+const express = require("express");
+const router = express.Router();
+const priceController = require("../controllers/priceController");
+
+router.get("/prices", priceController.getPrices);
+
+module.exports = router;
