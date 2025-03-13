b1df77471777c718d16690e648e49663c1f7166b
Ayoub <Fanaoui.25@gmail.com>
2025-03-13 22:35:46 +0100

    Configure Express for views and static files

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const express = require("express");
 const app = express();
 const PORT = 3000;
+const path = require("path");
+
+//configuration of the EJS template engine
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+//Middleware for static files
+app.use(express.static(path.join(__dirname, "public")));
+
+// Routes
+const priceRoutes = require("./routes/priceRoutes");
+app.use("/", priceRoutes);
 
 
-app.get("/",(req, res)=>{
-    res.send("Page index");
-});
 
-app.listen(PORT,()=>{
-    console.log('run server at port ', PORT);
-})
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
