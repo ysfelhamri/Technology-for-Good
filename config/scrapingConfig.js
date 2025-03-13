645216c64cfd88fca72c427d715df211a3a4f18c
Ayoub <Fanaoui.25@gmail.com>
2025-03-13 20:58:11 +0100

    Add scraping configuration file

--- config/scrapingConfig.js
+++ config/scrapingConfig.js
@@ -1,1 +1,14 @@
+const { name } = require("ejs");
+
 //to store the URLs and CSS selectors of sites to be scrapped.
+module.exports = {
+    sites : {
+        name : "ASwak salame",
+        url : "https://aswakassalam.com/",
+        selectors: {
+            product : ".product_name",
+            price : ".price",
+            category: " .category"
+        },
+    }
+};
